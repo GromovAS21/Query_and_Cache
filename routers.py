fe520d7a6db13866a0be4d83a2872f3cfa7b2ce5
Anton Gromov <seed14112@yandex.ru>
2025-04-19 16:28:20 +0300

    [+] добавление роутера для вывода списка последних торгов по фильтру

--- routers.py
+++ routers.py
@@ -1,4 +1,3 @@
-from datetime import date
 from typing import Annotated
 
 from fastapi import APIRouter, Depends, Query
@@ -7,7 +6,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.db_depends import get_db
 from models import TradingResults
-from schemas import SearchFilterTradingDate
+from schemas import SearchFilterTradingDate, SearchFilterTrading
 
 router = APIRouter(prefix="", tags=["spimex"])
 
@@ -35,7 +34,7 @@ async def get_last_trading_dates(db: Annotated[AsyncSession, Depends(get_db)], l
     return dates.all()
 
 
-@router.get("/trading_results")
+@router.get("/trading_dynamic")
 async def get_dynamics(db: Annotated[AsyncSession, Depends(get_db)],
                        data: SearchFilterTradingDate = Depends(SearchFilterTradingDate)
                        ):
@@ -47,7 +46,7 @@ async def get_dynamics(db: Annotated[AsyncSession, Depends(get_db)],
         data (SearchFilterTradingDate): Объект с данными для фильтрации и сортировки.
 
     Returns:
-        dict: Словарь с результатами фильтрации и сортировки.
+        dict: Словарь с результатами фильтрации.
     """
     tradings = await db.scalars(
         select(TradingResults).where(TradingResults.date.between(
@@ -57,4 +56,27 @@ async def get_dynamics(db: Annotated[AsyncSession, Depends(get_db)],
             TradingResults.delivery_type_id == data.delivery_type_id if data.delivery_type_id else True,
             TradingResults.delivery_basis_id == data.delivery_basis_id if data.delivery_basis_id else True,
         ))
+    return tradings.all()
+
+
+@router.get("/last_trading")
+async def get_trading_results(db: Annotated[AsyncSession, Depends(get_db)],
+                              data: SearchFilterTrading = Depends(SearchFilterTrading),
+                              limit: int = 5):
+    """
+    Возвращает список результатов торгов поcледние и фильтру.
+
+    Args:
+        db (AsyncSession): Сессия для работы с БД.
+        data (SearchFilterTrading): Объект с данными для фильтрации и сортировки.
+        limit (int): Количество последних торговых дней, которые нужно вернуть.
+
+    Returns:
+        dict: Словарь с результатами фильтрации.
+    """
+    tradings = await db.scalars(select(TradingResults).where(
+        TradingResults.oil_id == data.oil_id if data.oil_id else True,
+        TradingResults.delivery_type_id == data.delivery_type_id if data.delivery_type_id else True,
+        TradingResults.delivery_basis_id == data.delivery_basis_id if data.delivery_basis_id else True,
+    ).order_by(TradingResults.date.desc()).limit(limit))
     return tradings.all()
